ee38fa937aa9ea1b9e58f3daa59b1da262e13b3f
RichT <rrtrenchf@gmail.com>
2020-08-16 16:28:01 -0400

    Updates

---
+++ precache-manifest.aab4ec7fca28b48879cefeb57439bd71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7ba658e808b9a0b2c521e2bb8c1723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9e769a76c0784ac20fd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9227fc7089c2b00d4f2b",
+    "url": "/static/js/2.6b1ffd98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b1ffd98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e769a76c0784ac20fd",
+    "url": "/static/js/main.925faedf.chunk.js"
+  },
+  {
+    "revision": "75629d12a6015906c24e",
+    "url": "/static/js/runtime-main.cee7d932.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

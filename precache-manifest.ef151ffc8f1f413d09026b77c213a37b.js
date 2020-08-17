0ddf8c07abefbb603f1db7d13a2417977eb86e0a
RichT <rrtrenchf@gmail.com>
2020-08-17 19:54:15 -0400

    Updates

---
+++ precache-manifest.ef151ffc8f1f413d09026b77c213a37b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbf3bee4c2fc7470049ec4d1e4f94cf",
+    "url": "/react-mployee/index.html"
+  },
+  {
+    "revision": "a1f51b25ca174efbb85e",
+    "url": "/react-mployee/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9227fc7089c2b00d4f2b",
+    "url": "/react-mployee/static/js/2.6b1ffd98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-mployee/static/js/2.6b1ffd98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f51b25ca174efbb85e",
+    "url": "/react-mployee/static/js/main.870ce0ca.chunk.js"
+  },
+  {
+    "revision": "0c2bfaf64d95eeeb541e",
+    "url": "/react-mployee/static/js/runtime-main.c0951c5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-mployee/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0513518d179d84bab231e578f4c6acce.js"
+  "/react-mployee/precache-manifest.ef151ffc8f1f413d09026b77c213a37b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-mployee/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
